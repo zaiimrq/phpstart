c6937b19294aa838b70e94ac6682a1c1aba047c2
zaiimrq <zaiimrq@gmail.com>
2024-01-13 21:30:22 +0900

    add public assets feature

---
+++ app/controller/SampleController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\controller;
+
+class SampleController
+{
+    public function index()
+    {
+    }
+}
---
+++ public/assets/js/script.js
@@ -0,0 +1,5 @@
+
+let btn = document.getElementById('btn')
+btn.addEventListener('click', () => {
+    alert('JavaScript is working !')
+})
